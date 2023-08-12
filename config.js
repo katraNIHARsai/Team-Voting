21ad87450ce0710b633d203e804f8b62b476b206
katraNIHARsai <98602091+katraNIHARsai@users.noreply.github.com>
2023-08-12 14:33:48 +0530

    Update config.js
    I have added my config file.

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ import firebase from 'firebase';
 // add SDK Firebase
 var firebaseConfig = {
 
+  apiKey: "AIzaSyB6yHPC1reHbqyfLBYS8_fW_0N_2rybuiY",
+  authDomain: "class-58project.firebaseapp.com",
+  projectId: "class-58project",
+  storageBucket: "class-58project.appspot.com",
+  messagingSenderId: "204031798408",
+  appId: "1:204031798408:web:b10b779c791431ee1ec33d"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
